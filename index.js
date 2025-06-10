cb4855a25be2788112cfb444505fc00c0f12403f
ArtyomXXX <ambartsumyanartyomxxx@gmail.com>
2025-06-10 11:31:57 +0300

    minor updates

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ document.addEventListener("DOMContentLoaded", () => {
     const header = document.getElementById("header");
     const burgerMenu = document.getElementById("burger-menu");
     const menuMobile = document.querySelector("nav");
+    const overlay = document.getElementById("overlay");
 
     const toggleActiveClasses = (isActive) => {
         burgerMenu.classList.toggle("active", isActive);
         menuMobile.classList.toggle("active", isActive);
+        overlay.classList.toggle("active", isActive);
         document.body.style.overflow = isActive ? "hidden" : "";
     };
 
@@ -32,6 +34,27 @@ document.addEventListener("DOMContentLoaded", () => {
             toggleActiveClasses(false);
         }
     };
+
+    // Смещение скролл на -70px
+    const links = document.querySelectorAll('a[href^="#"]');
+
+    links.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault(); // Отменяем переход по ссылке
+
+            const targetId = this.getAttribute('href');
+            const targetElement = document.querySelector(targetId);
+
+            if (targetElement) {
+                const offsetPosition = targetElement.getBoundingClientRect().top + window.scrollY - 70;
+
+                window.scrollTo({
+                    top: offsetPosition,
+                    behavior: 'smooth',
+                });
+            }
+        });
+    });
 
     // Обработчик события прокрутки страницы
     window.addEventListener("scroll", handleHeader);
